31c3f5622ea209a3ddd82246d7f5c13ac78c6f8a
DEKU <mohamedmac.dioubate@gmail.com>
2026-09-18 21:36:35 +0000

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -12,6 +12,7 @@ export const pool =
   new Pool({
     connectionString: process.env.DATABASE_URL,
     max: 10,
+ ssl: { rejectUnauthorized: false },
   });
 
 if (process.env.NODE_ENV !== "production") {
